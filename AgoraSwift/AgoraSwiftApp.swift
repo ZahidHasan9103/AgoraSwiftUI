381de812ff692f05cda0be4a2ee7ee5ca822e890
Zahid Hasan <zahidhasan.9103@gmail.com>
2025-01-31 14:46:15 +0600

    Initial Commit

---
+++ AgoraSwift/AgoraSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgoraSwiftApp.swift
+//  AgoraSwift
+//
+//  Created by Hasan Zahid on 31/1/25.
+//
+
+import SwiftUI
+
+@main
+struct AgoraSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
